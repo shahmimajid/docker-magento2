a78e406fdc7ac5caf702e27eb242680d9b776153
Mohd Shahmi Abdul Majid <mshahmi@moneylion.com>
2018-08-03 16:48:29 +0800

    Change /bin/bash to php

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the Magento2 Unit Test
-docker exec $(docker ps | grep "docker-magento2-base" |awk '{print $1}') /bin/bash "su -l www-data && cd html && php -f vendor/bin/phpunit -- -c dev/tests/unit/phpunit.xml.dist"
+docker exec --user www-data $(docker ps | grep "docker-magento2-base" |awk '{print $1}') php "-f vendor/bin/phpunit -- -c dev/tests/unit/phpunit.xml.dist"
